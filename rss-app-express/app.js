1ec327618e048b02f151ec28ad7b3f29276c9776
Hoang21002 <dinhhoang026@gmail.com>
2023-11-24 00:08:14 +0700

    feat: Finish

--- rss-app-express/app.js
+++ rss-app-express/app.js
@@ -7,10 +7,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', async (req, res) => {
   try {
-    const feed = await parser.parseURL('https://vnexpress.net/rss/tin-moi-nhat.rss');
+    const rssURL = req.query.url || 'https://vnexpress.net/rss/tin-moi-nhat.rss';
+    const feed = await parser.parseURL(rssURL);
     res.render('index', { items: feed.items });
   } catch (error) {
     console.error('Error fetching RSS data:', error);
