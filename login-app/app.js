42edab93406123e90a16f38572da550b2b3ded43
Hoang21002 <dinhhoang026@gmail.com>
2023-11-24 01:45:20 +0700

    feat: Finish

---
+++ login-app/app.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const queryString = require('querystring');
+
+const students = [
+  { email: 'student1@example.com', password: 'pass1', name: 'John Doe', score: 85 },
+  { email: 'student2@example.com', password: 'pass2', name: 'Jane Smith', score: 92 },
+  // Thêm dữ liệu sinh viên khác nếu cần
+];
+
+const server = http.createServer((req, res) => {
+  const reqUrl = url.parse(req.url, true);
+  const pathname = reqUrl.pathname;
+
+  if (pathname === '/') {
+    const filePath = path.join(__dirname, 'public', 'index.html');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else if (pathname === '/login' && req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+
+    req.on('end', () => {
+      const formData = queryString.parse(body);
+      const email = formData.email.trim().toLowerCase(); // Chuẩn hóa email
+      const password = formData.password.trim(); // Chuẩn hóa password
+
+      const student = students.find(s => s.email === email && s.password === password);
+      if (student) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ success: true, student }));
+      } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ success: false, message: 'Sinh viên không có trong hệ thống.' }));
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Page not found');
+  }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
